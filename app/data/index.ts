9c767373bc3bd778706bc748b5dcce6258f4884d
Rafael Giro <me@rafaelgiro.dev>
2022-10-08 19:32:14 -0300

    feat: remove spirit blossom event

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,6 +1,4 @@
 import { patch1219 } from "./1219";
-import { changes } from "./changes";
-import { spiritBlossom } from "./spirit-blossom";
 import { malzahar } from "./malzahar";
 import { skins } from "./skins";
 import { bewitching } from "./bewitching";
@@ -9,8 +7,6 @@ import { empyrean } from "./empyrean";
 import { ksante } from "./ksante";
 
 export const data: Record<string, TimelineEvent> = {
-  spiritBlossom,
-  changes,
   patch1219,
   skins,
   bewitching,
